d2819b0258aaee36b505237934e5c57137df5b84
Projeto-9 <gileite176@gmail.com>
2020-06-26 15:39:23 -0300

    GIO 1.0

--- main.js
+++ main.js
@@ -1,15 +1,30 @@
 const {app,BrowserWindow} = require('electron');
 
 let mainWindow = null;
+let index = null;
+
 
 app.on('ready', () => {
   mainWindow = new BrowserWindow({
     width:500, 
+    height:1600,
+    resizable:false,
+    frame:false,
+    modal: true,
+    parent:index
+  });
+  index = new BrowserWindow({
+    width:500, 
     height:600,
     resizable:false,
     frame:false
-  });
-  mainWindow.loadURL('file://' + __dirname + '/materialize/login.html');
 
-  mainWindow.webContents.openDevTools();
+  });
+
+  index.loadURL('file://' + __dirname + '/view/index.html');
+
+  
+  mainWindow.loadURL('file://' + __dirname + '/view/login.html');
+  
+  //mainWindow.webContents.openDevTools();
 }); 
--- scripts/pagina.js
+++ scripts/pagina.js
@@ -4,20 +4,35 @@
 // });
 
 
-function login(){
-    var user = document.getElementById('username');
-    var pass = document.getElementById('pass');
-    console.log(user+' '+pass)
-    if(user == 'admin' && pass == 'admin'){
-        window.location = __dirname +'index.html'
-    }else{
-        alert('BURROOO')
+
+    function login(){
+        var user = document.getElementById("username");
+        var pass = document.getElementById("pass");
+    
+        //console.log(username.value+pass.value);
+        window.close()
+        if(user.value == "admin" && pass.value == "admin"){
+            localStorage.setItem("acesso", true);
+            alert("Usuário autenticado");           
+            window.close()
+        }else{
+            alert("Usuário ou senha invalidos!")
+        }
     }
-}
 
 
 
-function SolicitarCadastro(){
-    window.location = 
-}
+
+  function SolicitarCadastro(){
+    //alert('teste')
+    
+    //location.assign('../view/index.html');
+    //window.location.href = "C:\Users\Gio\Documents\Python\prog\PythonBurguer\view\index.html"
+    //Window.assign('file://' + __dirname + '/view/index.html');
+    //mainWindow.loadURL('./index.html');
+     //window.location.href = "./index.html" 
+     document.location.replace("C:\Users\Gio\Documents\Python\prog\PythonBurguer\view\index.html")
+    }
+
+
 
